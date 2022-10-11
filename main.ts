30667513e107903e7e9f6b5a75dfdecc861d0e29
m-11101017 <115517860+m-11101017@users.noreply.github.com>
2022-10-11 11:29:34 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,14 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.lightLevel())
 })
 basic.forever(function () {
-	
+    if (input.lightLevel() < 200) {
+        pins.digitalWritePin(DigitalPin.P2, 180)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    }
 })
